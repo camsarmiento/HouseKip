2dabb09de4976e5f335fac5f002a39f7aa497408
Camilo <camsarmiento@hotmail.com>
2018-06-26 23:13:11 +0100

    Detailm

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,26 @@
-const gulp = require ('gulp');
+const gulp =  require ('gulp');
+const imagemin = require('gulp-imagemin');
+
+
+
+
+//Move html file to build
+gulp.task('html',function(){
+	gulp.src('src/*.html')
+	.pipe(gulp.dest('build'));
+});
+
+//Minify imges 
+gulp.task('imageMin', function() {
+	gulp.src('src/img/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('build/img'))
+});
+
+
+
+
+
+gulp.task('default', function(){
+return console.log('funciona');
+});
